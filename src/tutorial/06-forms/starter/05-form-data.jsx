15ca7ee848cecb1d85109469f37dfb2e9428c521
thornicek <tomas.hornicek8@email.cz>
2024-02-02 13:18:24 -0500

    Section 6 Lesson 132 finished

--- src/tutorial/06-forms/starter/05-form-data.jsx
+++ src/tutorial/06-forms/starter/05-form-data.jsx
@@ -5,6 +5,10 @@ const UncontrolledInputs = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget)
+    const newUser = Object.fromEntries(formData)
+    setValue(value + 1)
+    e.currentTarget.reset()
   };
   return (
     <div>
@@ -24,7 +28,7 @@ const UncontrolledInputs = () => {
           </label>
           <input type='email' className='form-input' id='email' name='email' />
         </div>
-        {/* email */}
+        {/* password */}
         <div className='form-row'>
           <label htmlFor='password' className='form-label'>
             Password
